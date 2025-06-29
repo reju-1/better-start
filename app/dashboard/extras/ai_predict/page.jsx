905c42167c20cb00ae6ba3350f8935f08f08e948
Mohammad Abu Obaida Mullick <abu.obaida168@gmail.com>
2025-06-30 04:12:10 +0600

    Updated the EDA Prediction using CSV Module

---
+++ app/dashboard/extras/ai_predict/page.jsx
@@ -0,0 +1,9 @@
+import CsvAnalyzer from "./CsvAnalyzer";
+
+export default function HomePage() {
+  return (
+    <main className="min-h-screen bg-gray-50 p-10">
+      <CsvAnalyzer />
+    </main>
+  );
+}
