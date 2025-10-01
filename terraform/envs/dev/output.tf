40a35ae96eaeb7fe687fac2838bb5db819949bc2
reju-1 <rahmed1445@gmail.com>
2025-10-01 22:04:42 +0600

    chore(terraform): add outputs for VPC and EC2 instances

---
+++ terraform/envs/dev/output.tf
@@ -0,0 +1,26 @@
+# -------------------VPC-Related--------------------
+output "vpc_id" {
+  value       = module.vpc.vpc_id
+  description = "The ID of the VPC"
+}
+
+output "vpc_cidr" {
+  value       = module.vpc.vpc_cidr
+  description = "The CIDR block of the VPC"
+}
+
+# ----------------EC2 Related----------------------
+output "backend_public_ip" {
+  value       = module.ec2.backend_public_ip
+  description = "Public IP of backend instance"
+}
+
+output "frontend_public_ip" {
+  value       = module.ec2.frontend_public_ip
+  description = "Public IP of frontend instance"
+}
+
+output "worker_private_ip" {
+  value       = module.ec2.worker_private_ip
+  description = "Private IP of RabbitMQ worker instance"
+}
