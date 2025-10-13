a9a56c4ac102a23301aa4ca9c1bb5b2e56ba9567
reju-1 <rahmed1445@gmail.com>
2025-10-13 14:53:16 +0600

    chore(terraform): add necessary EC2 instance ID outputs

--- terraform/modules/ec2/output.tf
+++ terraform/modules/ec2/output.tf
@@ -1,3 +1,4 @@
+## --------------IP address-------------------
 output "backend_private_ip" {
   value       = aws_instance.backend.private_ip
   description = "Public IP of backend instance"
@@ -16,4 +17,25 @@ output "worker_private_ip" {
 output "bastion_host_public_ip" {
   value       = aws_instance.bastion.public_ip
   description = "Public ip of Bastion host"
+}
+
+## --------------Instance IDs-------------------
+output "backend_instance_id" {
+  value       = aws_instance.backend.id
+  description = "ID of the backend instance"
+}
+
+output "frontend_instance_id" {
+  value       = aws_instance.frontend.id
+  description = "ID of the frontend instance"
+}
+
+output "worker_instance_id" {
+  value       = aws_instance.worker.id
+  description = "ID of the worker instance"
+}
+
+output "bastion_instance_id" {
+  value       = aws_instance.bastion.id
+  description = "ID of the Bastion host instance"
 }
