e6009589e123b5ee1487bf0c6c1732dda1ff6672
reju-1 <rahmed1445@gmail.com>
2025-11-08 21:14:43 +0600

    chore(terraform): export ALB ARN from ALB module

--- terraform/modules/alb/outputs.tf
+++ terraform/modules/alb/outputs.tf
@@ -1,3 +1,8 @@
+output "alb_arn" {
+  description = "The ARN of the ALB"
+  value       = aws_lb.main.arn
+}
+
 output "alb_dns_name" {
   description = "DNS name of the load balancer"
   value       = aws_lb.main.dns_name
